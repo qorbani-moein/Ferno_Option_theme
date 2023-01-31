48881c7ccc05b85c4806fc14f434d8050eccc880
moein qorbani <szmp.info@gmail.com>
2023-01-31 14:55:53 +0330

    c1

--- option_theme.php
+++ option_theme.php
@@ -111,26 +111,27 @@ function script_page_cart(){
                 <span class="quantity_cart_minus" id="quantity_cart_minus_nth">-</span>
             </div>
             `;
+            var tag_input = document.getElementsByTagName("input");
             for(var i = 0 ; i < len_num_product ; i++){
                 document.getElementsByClassName("woolentor-cart-product-content")[i].innerHTML += quantity_product.split("nth").join(i);
                 
-                document.getElementById("quantity_cart_" + i)[i].value = document.getElementsByTagName("input").item(i).value;
-                document.getElementById("quantity_cart_" + i)[i].value = document.getElementsByTagName("input").item(i).value;
+                document.getElementById("quantity_cart_" + i)[i].value = tag_input[i].value;
+                document.getElementById("quantity_cart_" + i)[i].value = tag_input[i].value;
 
 
                 document.getElementById("quantity_cart_plus_" + i).onclick= function () {
                     document.getElementById("quantity_cart_" + i).value ++;
-                    document.getElementsByTagName("input").item(i).value = document.getElementById("quantity_cart_" + i).value;
+                    tag_input[i].value = document.getElementById("quantity_cart_" + i).value;
                 }
                 
                 document.getElementById("quantity_cart_minus_" + i).onclick= function () {
                     document.getElementById("quantity_cart_" + i).value --;
-                    document.getElementsByTagName("input").item(i).value = document.getElementById("quantity_cart_" + i).value;
+                    tag_input[i].value = document.getElementById("quantity_cart_" + i).value;
                 }
             }
 
         });
-        
+
     </script>
     ';
 }
