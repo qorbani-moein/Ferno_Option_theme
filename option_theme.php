077cd846a4200d6b4287b756e91a50a867ac7893
moein qorbani <szmp.info@gmail.com>
2023-02-06 15:38:53 +0330

    cc

--- option_theme.php
+++ option_theme.php
@@ -127,11 +127,24 @@ function product_archive (){
   // }
 
   $products = wc_get_products( array( 'status' => 'publish', 'limit' => -1 ) );
-  $thumbnail_id = get_woocommerce_term_meta( 22, 'thumbnail_id', true ); 
+  
+  global $wp_query;
+
+  // get the query object
+  $cat = $wp_query->get_queried_object();
+
+  // get the thumbnail id using the queried category term_id
+  $thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true ); 
+
+  // get the image URL
+  $image = wp_get_attachment_url( $thumbnail_id ); 
+
+  // print the IMG HTML
+  // echo "<img src='{$image}' alt='' width='762' height='365' />";
   ?>
 
   <div class="products-image-category">
-    <img class="img_product" src="<?php echo wp_get_attachment_url($thumbnail_id); ?>">
+    <img class="img_product" src="<?php echo $image; ?>">
   </div>
 
   <div class="products-slug">
