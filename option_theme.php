ba329b46b2a4a641a602b084a68c4c3c239aa0c9
moein qorbani <szmp.info@gmail.com>
2023-04-03 16:32:19 +0330

    cc

--- option_theme.php
+++ option_theme.php
@@ -565,9 +565,9 @@ function resource($elem, $type = null)
           //price
           var product_price = document.querySelectorAll(".product-price span bdi");
           var popup_price = document.querySelectorAll(".popup_product_price span");
-          // console.log(id);
+          console.log(id);
           // console.log(product_price[id].innerHTML);
-          popup_price[0].innerHTML = product_price[id].innerHTML;
+          popup_price[0].innerHTML = product_price[id - 5].innerHTML;
 
           for(var i = 0 ; i <= 40 ; i++){
             console.log(i + " - " + product_price[i].innerHTML);
