cfea0c610b1e959bc0da2ffd5e54357fcd210db7
moein qorbani <szmp.info@gmail.com>
2023-04-08 15:12:36 +0330

    cc

--- option_theme.php
+++ option_theme.php
@@ -406,18 +406,25 @@ function resource($elem, $type = null)
           
           //remove class menu actived
             // console.log("u.length: " + document.getElementsByClassName("item-category").length);
-            e.target.classList.add("products-slug-active");
             for(var u = 0 ; u <= document.getElementsByClassName("item-category").length - 1 ; u++){
               // console.log("u: " + u);
               document.getElementsByClassName("item-category")[u].classList.remove("products-slug-active");
-              // if(document.getElementsByClassName("item-category")[u].className.search == "products-slug-active"){
+              
+            }
+            
+            e.target.classList.add("products-slug-active");
+
+            //set page add product
+            for(var u = 0 ; u <= document.getElementsByClassName("item-category").length - 1 ; u++){
+              if(document.getElementsByClassName("item-category")[u].className.search == "products-slug-active"){
                 console.log(u);
                 const set_now = new Date();
                 
                 console.log(i + "-" + set_now.getTime());
                 sessionStorage.setItem("tab-clicked", u + "-" + set_now.getTime());
-              // }
+              }
             }
+
 
           // console.log("e.target.innerHTML: " + e.target.innerHTML);
             //filter category
