ce1edc30478891ed496092748ec367261fd281a8
moein qorbani <szmp.info@gmail.com>
2023-04-09 15:38:24 +0330

    cc

--- option_theme.php
+++ option_theme.php
@@ -505,11 +505,21 @@ function resource($elem, $type = null)
 
       if(url_site.indexOf("/?") > 0){
 
-        url_site = url_site.split("?");
-        location.replace = url_site[0];
+        // url_site = url_site.split("?");
+        // location.replace = url_site[0];
 
         // let url = new URL(location.href);
         // url.searchParams.delete(/[\[\]?*+|{}\\()@.\n\r]/);
+
+        const nextURL = "https://my-website.com/page_b";
+        const nextTitle = "My new page title";
+        const nextState = { additionalInformation: "Updated the URL with JS" };
+
+        // This will create a new entry in the browser s history, without reloading
+        window.history.pushState(nextState, nextTitle, nextURL);
+
+        // This will replace the current entry in the browser s history, without reloading
+        window.history.replaceState(nextState, nextTitle, nextURL);
 
       }
       //Click on tabs
