08192b1a77c2c3f727d285c785dfdc77f89cef93
moein qorbani <szmp.info@gmail.com>
2023-04-09 23:12:42 +0330

    cc

--- option_theme.php
+++ option_theme.php
@@ -630,7 +630,7 @@ function resource($elem, $type = null)
             var tmp_quantity_cart = document.getElementsByClassName("quantity_cart")[j].innerHTML
             tmp_quantity_cart = tmp_quantity_cart.split(`<span id="quantity_cart_add_to_card" hidden="">`);
 
-            if(tmp_quantity_cart.length > 0 && tmp_quantity_cart[1] != ""){
+            if(tmp_quantity_cart.length > 1){
               tmp_quantity_cart = tmp_quantity_cart[1].split("</span>");
               document.getElementsByClassName("input-text")[tmp_quantity_cart].value = document.getElementById("quantity_cart_" + j).value;
             }
