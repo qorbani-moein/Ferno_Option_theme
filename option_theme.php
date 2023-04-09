cb53a04d385b6f69b0242a236bf704d02ab78adb
moein qorbani <szmp.info@gmail.com>
2023-04-09 11:52:07 +0330

    cc

--- option_theme.php
+++ option_theme.php
@@ -524,41 +524,41 @@ function resource($elem, $type = null)
         /*window.scrollTo(100,200); 
         window.scrollBy(0,50);*/
 
-        document.getElementById("products-slug").scroll(200, 1000);
-        document.getElementsByClassName("item-category")[0].scroll(200, 1000);
-        document.getElementsByClassName("list-category")[0].scroll(200, 1000);
+        document.getElementById("products-slug").scroll(-200, -1000);
+        document.getElementsByClassName("item-category")[0].scroll(-200, -1000);
+        document.getElementsByClassName("list-category")[0].scroll(-200, -1000);
 
         document.getElementById("products-slug").scrollBy({
-          top: 1000,
-          left: 1000,
+          top: -1000,
+          left: -1000,
           behavior: "smooth",
         });
         document.getElementsByClassName("item-category")[0].scrollBy({
-          top: 1000,
-          left: 1000,
+          top: -1000,
+          left: -1000,
           behavior: "smooth",
         });
         document.getElementsByClassName("list-category")[0].scrollBy({
-          top: 1000,
-          left: 1000,
+          top: -1000,
+          left: -1000,
           behavior: "smooth",
         });
 
 
-        
+
         document.getElementById("products-slug").scrollTo({
-          top: 1000,
-          left: 1000,
+          top: -1000,
+          left: -1000,
           behavior: "smooth",
         });
         document.getElementsByClassName("item-category")[0].scrollTo({
-          top: 1000,
-          left: 1000,
+          top: -1000,
+          left: -1000,
           behavior: "smooth",
         });
         document.getElementsByClassName("list-category")[0].scrollTo({
-          top: 1000,
-          left: 1000,
+          top: -1000,
+          left: -1000,
           behavior: "smooth",
         });
 
