452407e64f431d2c03b833005fdfcae636080e21
Moein Qorbani <szmp.info@gmail.com>
2023-08-22 16:31:06 +0330

    ff

--- option_theme.php
+++ option_theme.php
@@ -678,8 +678,8 @@ function resource($elem, $type = null)
           
           //add or minus with click on + - and change input box
           if (id.search("plus")>0){
-            console.log("id_target.value" + document.getElementById("quantity_cart_" + id_target).value);
-            if(document.getElementById("quantity_cart_" + id_target).value == 0){document.getElementById("quantity_cart_" + id_target).value == 1}
+            console.log("id_target.value: " + document.getElementById("quantity_cart_" + id_target).value);
+            if(document.getElementById("quantity_cart_" + id_target).value == ""){document.getElementById("quantity_cart_" + id_target).value == 1}
             document.getElementById("quantity_cart_" + id_target).value ++;
           }
           else{
