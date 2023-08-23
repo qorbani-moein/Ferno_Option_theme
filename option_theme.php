04276a6550e0a4236b6c504ff30bcfe434efb794
Moein Qorbani <szmp.info@gmail.com>
2023-08-24 01:03:03 +0330

    ff

--- option_theme.php
+++ option_theme.php
@@ -498,9 +498,9 @@ function resource($elem, $type = null)
       // console.log("menubar: " + menubar + " - products_archive: " + products_archive + " - imagebar: " + imagebar + " - sticky: " + sticky);
       function sticky_menu() {
         // console.log("window.pageYOffset: " + window.pageYOffset);
-        // console.log("sticky: " + sticky);
+        console.log("sticky: " + sticky);
         if(sticky > 0){
-          if (window.pageYOffset >= 260) {
+          if (window.pageYOffset >= 270) {
             menubar.classList.add("sticky_menu");
             // products_archive.classList.add("products-archive-top");
           } else {
