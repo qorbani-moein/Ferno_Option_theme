3df9dd69dc8c28b6dd601d972210edbf5aead7ae
Moein Qorbani <szmp.info@gmail.com>
2023-09-05 00:32:45 +0330

    ff

--- option_theme.php
+++ option_theme.php
@@ -540,7 +540,7 @@ function resource($elem, $type = null)
             if(s_scroll[0] > 0){
               var xi = set_now_ss.getTime() - s_scroll[1];
               if(60000 > xi ){
-                setInterval(scrollY(),1000);
+                scrollY();
               }else{
                 localStorage.removeItem("scroll_mob");
               }
